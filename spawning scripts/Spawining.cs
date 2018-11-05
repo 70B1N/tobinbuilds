01730cf3db5ba0767b6370ad291251ec6226c91b
70B1N <tcspiller22@blakeschool.org>
2018-11-05 14:25:07 -0600

    Gamejamprototype

---
+++ spawning scripts/Spawining.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+
+    public GameObject[] enemies;
+    public Vector3 spawnvalues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+
+    int randEnemy;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (!stop)
+        {
+            randEnemy = Random.Range(0, 2);
+
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnvalues.x, spawnvalues.x), 1, Random.Range(-spawnvalues.z, spawnvalues.z));
+
+            Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+
+        }
+    }
+}
